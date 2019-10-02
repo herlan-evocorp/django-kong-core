a23894c2593c61c90edc7f6ba2c6423254d830fa
herlan <herlan@evocorp.com.br>
2019-10-02 10:53:09 -0300

    Fix bug in serializer

--- kong_core/serializers.py
+++ kong_core/serializers.py
@@ -3,10 +3,10 @@ from .models import Client
 
 
 class ClientSerializer(serializers.ModelSerializer):
-    id = serializers.CharField(read_only=True)
+    x_consumer_id = serializers.CharField(read_only=True)
 
     class Meta:
         model = Client
-        fields = ['id', 'x_consumer_id', 'x_consumer_custom_id',
+        fields = ['x_consumer_id', 'x_consumer_custom_id',
                   'x_consumer_username', 'x_authenticated_scope',
                   'x_authenticated_userid', 'x_anonymous_consumer']
