8c8333e40a004d68a86b901aa80a19c25fda4977
herlan <herlan@evocorp.com.br>
2019-10-02 08:40:53 -0300

    Serializer to Client

---
+++ kong_core/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Client
+
+
+class ClientSerializer(serializers.modelSerializer):
+    id = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Client
+        fields = ['id', 'x_consumer_id', 'x_consumer_custom_id',
+                  'x_consumer_username', 'x_authenticated_scope',
+                  'x_authenticated_userid', 'x_anonymous_consumer']
