f15c15336016adbef345fa6fbb4c0bbea6ad4e4a
herlan <herlan@evocorp.com.br>
2019-10-03 15:43:42 -0300

    Fix bug - quando tente criar o cliente novamente no middleware

--- kong_core/middleware.py
+++ kong_core/middleware.py
@@ -7,13 +7,19 @@ class KongClientMiddleware(MiddlewareMixin):
         headers = request.headers
 
         if headers is not None and headers.get('X-Consumer-ID', False):
-            client, created = Client.objects.get_or_create(
-                x_consumer_id=headers.get('X-Consumer-ID', None),
-                x_consumer_custom_id=headers.get('X-Consumer-Custom-ID', None),
-                x_consumer_username=headers.get('X-Consumer-Username', None),
-                x_authenticated_scope=headers.get(
-                    'X-Authenticated-Scope', None),
-                x_authenticated_userid=headers.get(
-                    'X-Authenticated-Userid', None),
-            )
+            client = Client.objects.get(pk=headers.get('X-Consumer-ID'))
+
+            if client is not None:
+                client, created = Client.objects.get_or_create(
+                    x_consumer_id=headers.get('X-Consumer-ID', None),
+                    x_consumer_custom_id=headers.get(
+                        'X-Consumer-Custom-ID', None),
+                    x_consumer_username=headers.get(
+                        'X-Consumer-Username', None),
+                    x_authenticated_scope=headers.get(
+                        'X-Authenticated-Scope', None),
+                    x_authenticated_userid=headers.get(
+                        'X-Authenticated-Userid', None),
+                )
+                
             request.client = client
