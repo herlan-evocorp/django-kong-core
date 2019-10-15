45713d3babf6b5f454f94b82f565d3737cbe7558
herlan <herlan@evocorp.com.br>
2019-10-15 13:32:17 -0300

    Fix error in info

--- kong_core/permissions.py
+++ kong_core/permissions.py
@@ -19,87 +19,87 @@ class DenyAny(AllowAny):
 class AllowAuthenticated(AllowAny):
     @staticmethod
     def has_node_permission(info, id):
-        return info.request.client is not None
+        return info.context.client is not None
 
     @staticmethod
     def has_mutation_permission(root, info, input):
-        return info.request.client is not None
+        return info.context.client is not None
 
     @staticmethod
     def has_filter_permission(info):
-        return info.request.client is not None
+        return info.context.client is not None
 
 
 # can access node
-class AllowClientNode(DenyAny):
+class AllowClienteNode(DenyAny):
     @staticmethod
     def has_node_permission(info, id):
-        if info.request.client is not None:
-            info.request.client.user_type == TipoUsuario.CLIENTE.name
+        if info.context.client is not None:
+            info.context.client.user_type == TipoUsuario.CLIENTE.name
         return False
 
 
 class AllowInstaladorNode(DenyAny):
     @staticmethod
     def has_node_permission(info, id):
-        if info.request.client is not None:
-            info.request.client.user_type == TipoUsuario.INSTALADOR.name
+        if info.context.client is not None:
+            info.context.client.user_type == TipoUsuario.INSTALADOR.name
         return False
 
 
 class AllowGestorNode(DenyAny):
     @staticmethod
     def has_node_permission(info, id):
-        if info.request.client is not None:
-            info.request.client.user_type == TipoUsuario.GESTOR.name
+        if info.context.client is not None:
+            info.context.client.user_type == TipoUsuario.GESTOR.name
         return False
 
 
 # can make mutation
-class AllowClientMutation(DenyAny):
+class AllowClienteMutation(DenyAny):
     @staticmethod
     def has_mutation_permission(root, info, input):
-        if info.request.client is not None:
-            info.request.client.user_type == TipoUsuario.CLIENTE.name
+        if info.context.client is not None:
+            info.context.client.user_type == TipoUsuario.CLIENTE.name
         return False
 
 
 class AllowInstaladorMutation(DenyAny):
     @staticmethod
     def has_mutation_permission(root, info, input):
-        if info.request.client is not None:
-            info.request.client.user_type == TipoUsuario.INSTALADOR.name
+        if info.context.client is not None:
+            info.context.client.user_type == TipoUsuario.INSTALADOR.name
         return False
 
 
 class AllowGestorMutation(DenyAny):
     @staticmethod
     def has_mutation_permission(root, info, input):
-        if info.request.client is not None:
-            info.request.client.user_type == TipoUsuario.GESTOR.name
+        if info.context.client is not None:
+            info.context.client.user_type == TipoUsuario.GESTOR.name
         return False
 
 
 # can filter
-class AllowClientFilter(DenyAny):
+class AllowClienteFilter(DenyAny):
     @staticmethod
     def has_filter_permission(info):
-        if info.request.client is not None:
-            info.request.client.user_type == TipoUsuario.CLIENTE.name
+        if info.context.client is not None:
+            info.context.client.user_type == TipoUsuario.CLIENTE.name
         return False
 
 
 class AllowInstaladorFilter(DenyAny):
     @staticmethod
     def has_filter_permission(info):
-        if info.request.client is not None:
-            info.request.client.user_type == TipoUsuario.INSTALADOR.name
+        if info.context.client is not None:
+            info.context.client.user_type == TipoUsuario.INSTALADOR.name
         return False
 
 
 class AllowGestorFilter(DenyAny):
     @staticmethod
     def has_filter_permission(info):
-        if info.request.client is not None:
-            info.request.client.user_type == TipoUsuario.GESTOR.name
+        if info.context.client is not None:
+            info.context.client.user_type == TipoUsuario.GESTOR.name
         return False
