a4119e3f89ae608f42d308227cfed0398d79128a
herlan <herlan@evocorp.com.br>
2019-10-16 11:33:13 -0300

    Fix inheritance mistakes

--- kong_core/mixins.py
+++ kong_core/mixins.py
@@ -8,7 +8,7 @@ class UserAuthNode(AuthNode):
 
     @classmethod
     def get_node(cls, info, id):
-        object_instance = super(AuthNode, cls).get_node(info, id)
+        object_instance = super(UserAuthNode, cls).get_node(info, id)
 
         if object_instance is not None and any((perm.has_node_permission(info, id) for perm in cls.permission_users)):
             return object_instance
@@ -20,7 +20,7 @@ class UserAuthFilter(AuthFilter):
 
     @classmethod
     def has_permission(cls, info):
-        object_instance = super(AuthFilter, cls).has_permission(info)
+        object_instance = super(UserAuthFilter, cls).has_permission(info)
 
         if object_instance is not None and any((perm.has_filter_permission(info) for perm in cls.permission_users)):
             return object_instance
@@ -32,4 +32,4 @@ class UserAuthMutation(AuthMutation):
 
     @classmethod
     def has_permission(cls, root, info, input):
-        return super(AuthMutation, cls) and any((perm.has_filter_permission(info) for perm in cls.permission_users))
+        return super(UserAuthMutation, cls) and any((perm.has_filter_permission(info) for perm in cls.permission_users))
