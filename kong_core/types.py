11e04b7319b9c1d7ce33eb7ee5909fddad67db25
herlan <herlan@evocorp.com.br>
2019-10-17 11:24:21 -0300

    1# try to fix get remote user parameters

--- kong_core/types.py
+++ kong_core/types.py
@@ -5,26 +5,26 @@ from kong_core.connections import TotalItemsConnection
 class UserRemoteType(graphene.ObjectType):
     id = graphene.ID()
     email = graphene.String()
-    full_name = graphene.String()
+    fullName = graphene.String()
     cpf = graphene.String()
     telefone = graphene.String()
     username = graphene.String()
-    first_name = graphene.String()
-    last_name = graphene.String()
+    firstName = graphene.String()
+    lastName = graphene.String()
     data_nascimento = graphene.String()
     cep = graphene.String()
     rua = graphene.String()
     numero = graphene.String()
     bairro = graphene.String()
     complemento = graphene.String()
-    cidade_name = graphene.String()
-    cidade_estado_name = graphene.String()
-    tipo_usuario = graphene.String()
+    cidadeName = graphene.String()
+    cidadeEstadoName = graphene.String()
+    tipoUsuario = graphene.String()
 
     class Meta:
-        fields = ['id', 'email', 'full_name', 'cpf', 'telefone', 'username',
-                  'first_name', 'last_name' 'data_nascimento', 'cep', 'rua',
-                  'numero', 'bairro', 'complemento', 'cidade_name', 'cidade_estado_name',
-                  'tipo_usuario', ]
+        fields = ['id', 'email', 'fullName', 'cpf', 'telefone', 'username',
+                  'firstName', 'lastName' 'data_nascimento', 'cep', 'rua',
+                  'numero', 'bairro', 'complemento', 'cidadeName', 'cidadeEstadoName',
+                  'tipoUsuario', ]
         connection_class = TotalItemsConnection
         use_connection = True
