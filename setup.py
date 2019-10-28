6cba3f077bbe8c084c4c1a6c0645e9c75d5d3b9b
gerson-evocorp <47366303+gerson-evocorp@users.noreply.github.com>
2019-10-28 13:02:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-kong-core',
     version='0.8.5.6',
-    packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
     description='Django Kong Core',
