2a23c4f8a461f4e304ec1f8da807b2093fa4f151
gerson-evocorp <47366303+gerson-evocorp@users.noreply.github.com>
2019-10-29 20:55:21 -0300

    Update errors.py

--- kong_core/errors.py
+++ kong_core/errors.py
@@ -13,3 +13,11 @@ class GraphQLError(BaseGraphQLError):
 
 class PermissionDenied(GraphQLError):
     message = _("Você não tem permissão para isso!")
+    
+    
+class HasRelationsException(Exception):
+    def __init__(self, object_relation, object_related):
+        self.object_relation = object_relation
+        self.object_related = object_related
+    def __str__(self):
+        return "Este(a) {} está vinculado a um(a) {}".format(self.object_relation, self.object_related)
