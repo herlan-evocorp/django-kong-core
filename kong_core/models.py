08c207c470b752f805313a932ca99c62fc0936a4
gerson-evocorp <gerson@evocorp.com.br>
2019-12-26 13:53:49 -0300

    corrections add in permissions

--- kong_core/models.py
+++ kong_core/models.py
@@ -67,7 +67,6 @@ class Client(BaseModel):
     x_authenticated_scope = models.CharField(max_length=256, null=True)
     x_authenticated_userid = models.PositiveIntegerField(null=True)
     x_anonymous_consumer = models.BooleanField(default=False)
-    user_type = models.CharField(max_length=256, null=True)
 
     def __str__(self):
         return self.x_consumer_id
