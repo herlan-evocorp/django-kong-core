aeb0b18692536b332763e845ecfafffc1229d600
gerson-evocorp <47366303+gerson-evocorp@users.noreply.github.com>
2020-01-06 16:15:21 -0300

    u

--- kong_core/utils.py
+++ kong_core/utils.py
@@ -4,3 +4,7 @@ class ChoiceEnum(Enum):
     @classmethod
     def choices(cls):
         return tuple((i.name, i.value) for i in cls)
+    @classmethod
+    def to_list(cls):
+        return [{'key':i.name, 'value': i.value} for i in cls]
+
