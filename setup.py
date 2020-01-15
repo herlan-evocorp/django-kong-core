6baabc5fce5d38674e88ae9ae0102ff473f90f45
gerson-evocorp <gerson@evocorp.com.br>
2020-01-15 13:20:13 -0300

    add class ApiError

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def create_mo_files():
 
 setup(
     name='django-kong-core',
-    version='0.9.8',
+    version='0.9.9',
     license='BSD License',  # example license
     description='Django Kong Core',
     long_description=README,
